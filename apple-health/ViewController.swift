62cf9399cb628f5748202c577f27af9ab023950e
Vadym Yatsyuk <vadym.yatsyuk@gmail.com>
2017-12-14 22:54:04 +0100

    init

---
+++ apple-health/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  apple-health
+//
+//  Created by Vadim Dez on 14.12.17.
+//  Copyright © 2017 Vadim Dez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
